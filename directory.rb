4887679d281e1b8db71908e4d9adc2499875927e
alakijayo <alakijaayo@gmail.com>
2018-08-30 17:32:14 +0100

    Exercise 8.5

--- directory.rb
+++ directory.rb
@@ -8,7 +8,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: "Causing chaos", birth: "DOB", height: "6ft 3in"}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
@@ -24,7 +24,7 @@ end
 def print(students)
     index_var = 0
     while index_var < students.length
-      puts "#{index_var + 1}. #{students[index_var][:name]} (#{students[index_var][:cohort]} cohort)"
+      puts "#{index_var + 1}. #{students[index_var][:name]} (#{students[index_var][:cohort]} cohort), birth: #{students[index_var][:birth]}, height: #{students[index_var][:height]}, hobby: #{students[index_var][:hobby]}"
       index_var += 1
     end
 end
